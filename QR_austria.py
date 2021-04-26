86006622e2976dfd834281ac409dc0f7c5381fb9
Caynexes <fbxx@live.de>
2021-04-26 23:51:25 +0200

    Add files via upload

--- QR_austria.py
+++ QR_austria.py
@@ -4,10 +4,10 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 
-# The QR code id is the input paramenter of the function
-def qr_validation_vienna(id):
+# The QR code id is the input parameter of the function
+def qr_validation_austria(id):
     # issuing authority
-    authority = "City of Vienna"
+    authority = "State of Austria"
 
     # building the final url
     url = id
@@ -25,11 +25,14 @@ def qr_validation_vienna(id):
         QR_html = BeautifulSoup(qr_validation.text, "html.parser")
 
         # extraction of the required information based on the qr code url
-        data = QR_html.find_all("p")
+        data = QR_html.find_all("dd")
         first_name = data[0].text
         last_name = data[1].text
         date_of_birth = data[2].text
-        test_date = data[3].text
+        # several operations to get the test date only
+        test_date = data[3]
+        test_date = test_date.find_all("span")
+        test_date = test_date[0].text
 
         # adding a validation time stamp
         current_date = datetime.now()
@@ -43,5 +46,5 @@ def qr_validation_vienna(id):
 
     # city vienna webserver is offline (does not mean that the result is not valid)
     else:
-        offline = "the city vienna webserver is offline"
+        offline = "the State of Austria webserver is offline"
         return offline, offline, offline, offline, offline, offline, offline, offline
