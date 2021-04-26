b3b6a917b98e2867ef156b5fd932ec0736b259da
Caynexes <fbxx@live.de>
2021-04-26 23:44:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import QR_vienna
-
+import QR_austria
+import URL_Identification
 import audittrail
 
 app = FastAPI()
@@ -8,17 +9,34 @@ app = FastAPI()
 password = "abcd"
 
 
+@app.get("/")
+async def welcome():
+    return {"message": "The service is up and running"}
+
 @app.get("/verification/")
 async def qr_validation(qr_url: str = "The QR Code you want to validate", audit_trail: bool = True,
                         apikey: str = "Your personal API key"):
 
+#We have to check if the qr_url is containing querry arguments e.g. & signs, if so, we have to replace them with %26
+
     if apikey != password:
         return {"message": "The API key validation failed"}
 
+    URL_validation_position = URL_Identification.URL(qr_url)
+
     #The API parameter equals our internal ID
     id = qr_url
-    #The ID will be handed over to the QR code validation function and the output is our validation result
-    validation_result = QR_vienna.qr_validation_vienna(id)
+
+    if URL_validation_position == 0:
+        validation_result = QR_austria.qr_validation_austria(id)
+        #return {"message": "URL_validation_position 0"}
+
+    # The ID will be handed over to the QR code validation function and the output is our validation result
+    if URL_validation_position == 1:
+        validation_result = QR_vienna.qr_validation_vienna(id)
+
+    if URL_validation_position != 0 and URL_validation_position != 1:
+        return {"message": "Our service is not providing the provided QR Code so far"}
 
 
     #The audit trail will only be created if the querry paramenter "audit_trail" is true (default value)
